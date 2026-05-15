4c40f2528d24ff79ed7bfd1b19c2fb83ae45ac9b
ShcherbakovP <62602710+ShcherbakovP@users.noreply.github.com>
2026-05-15 10:45:41 +0100

    Update rev3.c

--- keyboards/ergohaven/macropad/rev3/rev3.c
+++ keyboards/ergohaven/macropad/rev3/rev3.c
@@ -1,24 +1,20 @@
-#include "display.h"
-#include "ergohaven.h"
-#include "src/display/eh_display.h"
+  #include "display.h"
+  #include "ergohaven.h"
+  #include "src/display/eh_display.h"
+  #include "src/eh_settings.h"
 
-void housekeeping_task_user(void) {
-    display_housekeeping_task();
-}
-
-void keyboard_post_init_user(void) {
-    display_init_kb();
-}
-
-#include "src/eh_settings.h"
-
-  uint32_t 
-  get_lcd_timeout_ms(void) {
-      return 6UL * 60 * 60 * 1000;     // 6 часов
+  void housekeeping_task_user(void) {
+      display_housekeeping_task();
   }
 
-  uint8_t 
-  get_lcd_timeout_mins(void) {
-      return 255;   // на случай
-  если где-то проверяется uint8_t
+  void keyboard_post_init_user(void) {
+      display_init_kb();
+  }
+
+  uint32_t get_lcd_timeout_ms(void) {
+      return 6UL * 60 * 60 * 1000;
+  }
+
+  uint8_t get_lcd_timeout_mins(void) {
+      return 255;
   }
